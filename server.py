89007e1a842b6689d103f1ce573b501cd4046f5f
Anand <anand.cse598@gmail.com>
2017-11-07 19:29:09 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import os
 from flask import Flask, request, redirect
 from twilio.jwt.access_token import AccessToken, VoiceGrant
@@ -16,50 +19,57 @@ CALLER_ID = 'quick_start'
 
 app = Flask(__name__)
 
+
 @app.route('/accessToken')
 def token():
-  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
-  api_key = os.environ.get("API_KEY", API_KEY)
-  api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
-  push_credential_sid = os.environ.get("PUSH_CREDENTIAL_SID", PUSH_CREDENTIAL_SID)
-  app_sid = os.environ.get("APP_SID", APP_SID)
+    account_sid = os.environ.get('ACCOUNT_SID', ACCOUNT_SID)
+    api_key = os.environ.get('API_KEY', API_KEY)
+    api_key_secret = os.environ.get('API_KEY_SECRET', API_KEY_SECRET)
+    push_credential_sid = os.environ.get('PUSH_CREDENTIAL_SID',
+            PUSH_CREDENTIAL_SID)
+    app_sid = os.environ.get('APP_SID', APP_SID)
 
-  grant = VoiceGrant(
-    push_credential_sid=push_credential_sid,
-    outgoing_application_sid=app_sid
-  )
+    grant = VoiceGrant(push_credential_sid=push_credential_sid,
+                       outgoing_application_sid=app_sid)
 
-  token = AccessToken(account_sid, api_key, api_key_secret, IDENTITY)
-  token.add_grant(grant)
+    token = AccessToken(account_sid, api_key, api_key_secret, IDENTITY)
+    token.add_grant(grant)
 
-  return str(token)
+    return str(token)
+
 
 @app.route('/outgoing', methods=['GET', 'POST'])
 def outgoing():
-  resp = twilio.twiml.Response()
-  resp.say("Congratulations! You have made your first oubound call! Good bye.")
-  return str(resp)
+    resp = twilio.twiml.Response()
+    resp.say('Congratulations! You have made your first oubound call! Good bye.'
+             )
+    return str(resp)
+
 
 @app.route('/incoming', methods=['GET', 'POST'])
 def incoming():
-  resp = twilio.twiml.Response()
-  resp.say("Congratulations! You have received your first inbound call! Good bye.")
-  return str(resp)
+    resp = twilio.twiml.Response()
+    resp.say('Congratulations! You have received your first inbound call! Good bye.'
+             )
+    return str(resp)
+
 
 @app.route('/placeCall', methods=['GET', 'POST'])
 def placeCall():
-  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
-  api_key = os.environ.get("API_KEY", API_KEY)
-  api_key_secret = os.environ.get("API_KEY_SECRET", API_KEY_SECRET)
+    account_sid = os.environ.get('ACCOUNT_SID', ACCOUNT_SID)
+    api_key = os.environ.get('API_KEY', API_KEY)
+    api_key_secret = os.environ.get('API_KEY_SECRET', API_KEY_SECRET)
 
-  client = Client(api_key, api_key_secret, account_sid)
-  call = client.calls.create(url=request.url_root + 'incoming', to='client:' + IDENTITY, from_='client:' + CALLER_ID)
-  return str(call.sid)
+    client = Client(api_key, api_key_secret, account_sid)
+    call = client.calls.create(url=request.url_root + 'incoming',
+                               to='client:' + IDENTITY, from_='client:'
+                               + CALLER_ID)
+    return str(call.sid)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-  
-  
+
 # response = twilio.twiml.VoiceResponse()
 # response.say("Welcome to Teletext Holidays")
 # dial = twilio.twiml.Dial()
@@ -67,19 +77,25 @@ def welcome():
 # response.append(dial)
 # response.say('Goodbye')
 
-response = VoiceResponse()
-response.say("Welcome to Teletext Holidays")
-dial = Dial(caller_id='+918017565056')
+# resp = twilio.twiml.VoiceResponse()
+# resp.say("Welcome to Teletext Holidays")
+# dial = Dial(caller_id='+918017565056')
+# dial.number('+917799579996')
+# resp.append(dial)
+
+## # return str(response)
+
+    resp = twilio.twiml.VoiceResponse()
+    resp.say('Welcome to Teletext Holidays United Kingdom')
+    return str(resp)
+
+dial = Dial(caller_id='+917799579996')
 dial.number('+917799579996')
 response.append(dial)
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
-# # return str(response)
- 
-#   resp = twilio.twiml.Response()
-#   resp.say("Welcome to Teletext Holidays United Kingdom")
-#   return str(resp)
 
-if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+			
